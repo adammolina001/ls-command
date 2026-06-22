a3db6903055e44fa8595c840934a9af13386dab1
Adam <adam.molina001@gmail.com>
2026-06-22 15:07:35 +0200

    chores: code factorisation and better use of lib function

--- ls.c
+++ ls.c
@@ -17,6 +17,21 @@
 
 
 
+void set_str_perms(struct stat file_info, char* perms) {
+
+    perms[0] = S_ISDIR(file_info.st_mode) ? 'd' : '-';
+    perms[1] = S_IRUSR & file_info.st_mode ? 'r' : '-';
+    perms[2] = S_IWUSR & file_info.st_mode ? 'w' : '-';
+    perms[3] = S_IXUSR & file_info.st_mode ? 'x' : '-';
+    perms[4] = S_IRGRP & file_info.st_mode ? 'r' : '-';
+    perms[5] = S_IWGRP & file_info.st_mode ? 'w' : '-';
+    perms[6] = S_IXGRP & file_info.st_mode ? 'x' : '-';
+    perms[7] = S_IROTH & file_info.st_mode ? 'r' : '-';
+    perms[8] = S_IWOTH & file_info.st_mode ? 'w' : '-';
+    perms[9] = S_IXOTH & file_info.st_mode ? 'x' : '-';
+    perms[10] = '\0';
+}
+
 void ls(char* target_dir, int show_hidden, int show_long) {
     DIR *dir;
     struct dirent *dir_file;
@@ -24,6 +39,7 @@ void ls(char* target_dir, int show_hidden, int show_long) {
     dir = opendir(target_dir); 
 
         dir_file = readdir(dir);
+        
         if (show_long) printf("permissions  liens  proprietaire  groupe  taille  date  nom\n");
 
         while(dir_file != NULL) {
@@ -43,23 +59,12 @@ void ls(char* target_dir, int show_hidden, int show_long) {
                 }
 
                 //Construction de la date 
-                size_t size_max = 20; //9 = september + "  " + day + " " + hour + \0
-                char* file_time = malloc(20);
-                strftime(file_time, size_max, "%B  %d %R", gmtime(&file_info.st_mtime));   
+                char file_time[20]; //9 = september + "  " + day + " " + hour + \0
+                strftime(file_time, 20, "%B  %d %R", localtime(&file_info.st_mtime));   
                 
                 //permissions
                 char perms[11]; //1directory 3owner 3group 3others
-                perms[0] = S_ISDIR(file_info.st_mode) ? 'd' : '-';
-                perms[1] = S_IRUSR & file_info.st_mode ? 'r' : '-';
-                perms[2] = S_IWUSR & file_info.st_mode ? 'w' : '-';
-                perms[3] = S_IXUSR & file_info.st_mode ? 'x' : '-';
-                perms[4] = S_IRGRP & file_info.st_mode ? 'r' : '-';
-                perms[5] = S_IWGRP & file_info.st_mode ? 'w' : '-';
-                perms[6] = S_IXGRP & file_info.st_mode ? 'x' : '-';
-                perms[7] = S_IROTH & file_info.st_mode ? 'r' : '-';
-                perms[8] = S_IWOTH & file_info.st_mode ? 'w' : '-';
-                perms[9] = S_IXOTH & file_info.st_mode ? 'x' : '-';
-                perms[10] = '\0';
+                set_str_perms(file_info, perms);
 
                 printf("%s  %lu  %s  %s  %8ld  %20s  %s\n", perms, file_info.st_nlink, getpwuid(file_info.st_uid)->pw_name, getgrgid(file_info.st_gid)->gr_name, file_info.st_size, file_time, dir_file->d_name);
 
@@ -87,10 +92,10 @@ int main(int argc, char** argv) {
     }
 
     while(cmp < argc) {
-        if (strcmp(argv[cmp], "-a") == 0) {
-            show_hidden = 1;
-        }
-        if (strcmp(argv[cmp], "-l") == 0) {
+        if (strcmp(argv[cmp], "-a") == 0) show_hidden = 1;
+        if (strcmp(argv[cmp], "-l") == 0) show_long = 1;
+        if (strcmp(argv[cmp], "-la") == 0 || strcmp(argv[cmp], "-al") == 0) {
+            show_hidden = 1; 
             show_long = 1;
         }
         cmp++;
